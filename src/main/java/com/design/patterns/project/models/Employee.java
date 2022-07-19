64fbc427b04a6f6b18448b3c903c9d46ee23f1dd
bvegaM <vegabryam40@gmail.com>
2022-07-19 12:24:46 -0500

    agregando modelo de empleados y dosis

---
+++ src/main/java/com/design/patterns/project/models/Employee.java
@@ -0,0 +1,66 @@
+package com.design.patterns.project.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity()
+@Table(name = "t_employees")
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull(message = "El dni es requerido")
+    @Column(name = "emp_dni",length = 10,unique = true)
+    @Size(min = 10, max = 10, message = "El dni debe tener 10 digitos")
+    @Pattern(regexp = "^[0-9]{10}$", message = "El dni debe tener 10 digitos")
+    private String dni;
+
+    @NotNull(message = "El nombre es requerido")
+    @Column(name = "emp_first_name")
+    @Pattern(regexp = "^[a-zA-Z\\s]+$", message = "El nombre solo debe contener letras")
+    private String firstName;
+
+    @NotNull(message = "El apellido es requerido")
+    @Column(name = "emp_last_name")
+    @Pattern(regexp = "^[a-zA-Z\\s]+$", message = "El nombre solo debe contener letras")
+    private String lastName;
+
+    @NotNull(message = "El email es requerido")
+    @Column(name = "emp_email")
+    @Pattern(regexp = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$", message = "El email no es valido")
+    private String email;
+
+    @Column(name = "emp_date_birth")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date dateBirth;
+
+    @Column(name = "emp_address")
+    private String address;
+
+    @Column(name = "emp_phone_number")
+    @Pattern(regexp = "^[0-9]{10}$", message = "El numero de telefono debe tener 10 digitos")
+    private String phone;
+
+    @Column(name = "emp_status_vac")
+    private Boolean vaccineStatus;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<Dose> doses;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "emp_usr_id", referencedColumnName = "usr_id")
+    private User user;
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,6 +16,40 @@ create table t_users(
     constraint "fk_user_role"
         foreign key ("usr_role_id")
             references T_ROLES ("role_id")
-            on delete no action
-            on update no action
+    on delete no action
+    on update no action
+);
+
+create table t_employees(
+    "emp_id" serial not null,
+    "emp_dni" varchar(10) not null,
+    "emp_first_name" varchar(150) not null,
+    "emp_last_name" varchar(150) not null,
+    "emp_email" varchar(100) not null,
+    "emp_date_birth" timestamp null,
+    "emp_phone_number" varchar(10) null,
+    "emp_address" varchar(255) null,
+    "emp_status_vac" boolean null,
+    "emp_usr_id" int not null,
+    primary key ("emp_id"),
+    unique("emp_dni"),
+    constraint "fk_employee_user"
+        foreign key ("emp_usr_id")
+            references T_USERS ("usr_id")
+    on delete no action
+    on update no action
+);
+
+create table t_doses(
+    "dose_id" serial not null,
+    "dose_number" int not null,
+    "dose_vaccine" varchar not null,
+    "dose_date" timestamp not null,
+    "dose_emp_id" int not null,
+    primary key ("dose_id"),
+    constraint "fk_dose_employee"
+        foreign key ("dose_emp_id")
+            references T_EMPLOYEES ("emp_id")
+    on delete no action
+    on update no action
 );
